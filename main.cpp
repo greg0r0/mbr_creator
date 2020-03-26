4ee91d008e47e9226f6cd81569645244a4927787
greg0r0 <game_grego@mail.ru>
2020-03-26 20:14:05 +0300

    Added LBA support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "mbr.h"
 
 
@@ -10,24 +12,27 @@ struct lol
     char b;
 };
 
-struct mbr create_mbr(int partitions, int is_bootable);
+struct mbr create_mbr(int partitions, int is_bootable, int block_size);
+int getFileSize(const char *fd);
 
 int main(int argc, char* argv[]){
-
-    struct mbr mbrr = create_mbr(1, true);
-
-    std::cout<<sizeof(struct mbr)<<std::endl;
     FILE * fd;
-
     fd = fopen(argv[1], "r+b");
 
+    int file_size = getFileSize(argv[1]);
+
+
+    struct mbr mbrr = create_mbr(atoi(argv[2]), true, file_size);
     fwrite(&mbrr, sizeof(mbrr), 1, fd);
 
     fclose(fd);
     return 0;
 }
 
-struct mbr create_mbr(int partitions, int is_bootable){
+struct mbr create_mbr(int partitions, int is_bootable, int file_size){
+    int sectors_len = ((file_size / 512)) / partitions;
+
+    std::cout << "Sectors per partition: "<<sectors_len << std::endl;
     struct mbr *new_mbr;
 
     new_mbr = (struct mbr *)malloc(sizeof(struct mbr));
@@ -35,18 +40,25 @@ struct mbr create_mbr(int partitions, int is_bootable){
     new_mbr->signature = 0x0;
     new_mbr->vvoid     = 0;
     if (partitions <= 4 ){
-        for (int i =0; i<partitions; i++){
-            new_mbr->partition[i].status         = (is_bootable != 0) ? BOOTABLE_MBR : 0;
+        for (int i = 0; i<partitions; i++){
+            std::cout<<"Creating partition " << i << std::endl;
+            new_mbr->partition[i].status         = (is_bootable != 0 && i == 0) ? BOOTABLE_MBR : 0;
             new_mbr->partition[i].first_sector   = (struct chs){0, 0, 0};
             new_mbr->partition[i].partition_type = 0x01;
             new_mbr->partition[i].end_sector     = (struct chs){0, 0, 0};
-            new_mbr->partition[i].first_sector_lba = 0;
-            new_mbr->partition[i].sectors = 0;
+            new_mbr->partition[i].first_sector_lba = 1+sectors_len*i;
+            new_mbr->partition[i].sectors = sectors_len;
         }
     } else {
-        *new_mbr = create_mbr(4, is_bootable);
+        *new_mbr = create_mbr(4, is_bootable, file_size);
     }
     new_mbr->mbr_signature = MBR_SIGNATURE;
 
     return *new_mbr;
+}
+
+int getFileSize(const char * fd){
+    struct stat stat_buf;
+    int res = stat(fd, &stat_buf);
+    return stat_buf.st_size;
 }
